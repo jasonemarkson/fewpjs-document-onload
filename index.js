9e3920de48ca823be9701424f3648e88cb4193d9
jasonemarkson <jasonmarkson76@gmail.com>
2021-05-21 13:10:44 -0400

    commit tree bb2e9158129f1c6b5821765c7c51a0ba320e55b6

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log('The DOM has loaded')
+    newText();
+})
+
+function newText() {
+    const p = document.getElementById('text')
+    p.innerHTML = "This is really cool!"
+}
